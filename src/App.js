23a769493c4aa51ce31e7c4e59ff3d14905934a5
Ankitfullstack <ankit12.at9@gmail.com>
2025-10-15 16:27:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import CourseGoal from "./components/CourseGoal";
-import Heading from "./components/Heading";
+// import CourseGoal from "./components/CourseGoal";
+// import Heading from "./components/Heading";
+// import "./App.css";
+// function App() {
+//   return (
+//     <div className="appContainer">
+//       <h1> TIME TO PRACTICE</h1>
+//       <p id="paragraph">One course, many goals!🎯 </p>
+//       <CourseGoal title="LEARN REACT" description="IN DEPTH" />
+//       <CourseGoal
+//         title="PRACTICE"
+//         description="PRACTICE WORKING WITH REACT COMPONENTS"
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Card from "./components/Card";
 import "./App.css";
+
 function App() {
   return (
     <div className="appContainer">
-      <h1> TIME TO PRACTICE</h1>
-      <p id="paragraph">One course, many goals!🎯 </p>
-      <CourseGoal title="LEARN REACT" description="IN DEPTH" />
-      <CourseGoal
-        title="PRACTICE"
-        description="PRACTICE WORKING WITH REACT COMPONENTS"
-      />
+      <Card className="name.card" name="Maria Miles">
+        <p id="card-body">
+          Maria is a professor of Computer Science at the University of
+          Illinois.
+        </p>
+        <p>
+          <a id="card-footer" href="mailto:blake@example.com">
+            Email Maria
+          </a>
+        </p>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card({ name, children }) {
+  return (
+    <div>
+      <h2 className="name-card">{name}</h2>
+      <div>{children}</div>
+    </div>
+  );
+}
+export default Card;
