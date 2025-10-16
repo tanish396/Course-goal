7b21a0c0a73c47f7546c94a79e5e39d9f48906ea
Ankitfullstack <ankit12.at9@gmail.com>
2025-10-16 12:41:31 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -17,25 +17,88 @@
 
 // export default App;
 
-import Card from "./components/Card";
-import "./App.css";
+// import Card from "./components/Card";
+// import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="appContainer">
+//       <Card name="Maria Miles">
+//         <p id="card-body">
+//           Maria is a professor of Computer Science at the University of
+//           Illinois.
+//         </p>
+//         <p>
+//           <a id="card-footer" href="mailto:blake@example.com">
+//             Email Maria
+//           </a>
+//         </p>
+//       </Card>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+
+const user = {
+  email: "",
+  password: "",
+  loggedIn: false,
+};
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (user.email === email) {
+      setError("Email Id is already exists");
+      return;
+    }
+    user.email = email;
+    user.password = password;
+    user.loggedIn = true;
+    setError("");
+
+    alert("Login Successful");
+    setEmail("");
+    setPassword("");
+  };
   return (
-    <div className="appContainer">
-      <Card className="name.card" name="Maria Miles">
-        <p id="card-body">
-          Maria is a professor of Computer Science at the University of
-          Illinois.
-        </p>
-        <p>
-          <a id="card-footer" href="mailto:blake@example.com">
-            Email Maria
-          </a>
-        </p>
-      </Card>
+    <div className="container mt-5">
+      <div className="mb-3">
+        <label className="form-label">Email</label>
+        <div className="col-sm-10">
+          <input
+            type="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          ></input>
+        </div>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Password</label>
+        <div className="col-sm-10">
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          ></input>
+        </div>
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <button type="button" className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
 
-export default App;
+export default Login;
